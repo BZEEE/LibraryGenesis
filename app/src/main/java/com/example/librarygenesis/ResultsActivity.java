07dcaae5ef3d248a41d864701c96d4d2b95b8343
bjzuk <bjzukows@ualberta.ca>
2019-05-24 00:29:21 -0400

    added recycler view todisplay multiple website links, now have to figure out how to populate the recycler view with items

--- app/src/main/java/com/example/librarygenesis/ResultsActivity.java
+++ app/src/main/java/com/example/librarygenesis/ResultsActivity.java
@@ -2,12 +2,32 @@ package com.example.librarygenesis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.example.librarygenesis.SearchResultsAdapter;
 
 public class ResultsActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+//        recyclerView = findViewById(R.id.results_recycler_view);
+//
+//        // use this setting to improve performance if you know that changes
+//        // in content do not change the layout size of the RecyclerView
+//        recyclerView.setHasFixedSize(true);
+//
+//        // use a linear layout manager
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        // specify an adapter (see also next example)
+//        mAdapter = new SearchResultsAdapter(myDataset);
+//        recyclerView.setAdapter(mAdapter);
     }
 }
